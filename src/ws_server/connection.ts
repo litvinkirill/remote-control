f55f0381a76f33acdb44ca078753af305094bee7
Kirill Litvin <litvin.kirill.m@mail.ru>
2022-06-26 23:27:22 +0300

    fix: some logica

--- src/ws_server/connection.ts
+++ src/ws_server/connection.ts
@@ -1,26 +1,31 @@
-import { createWebSocketStream } from 'ws';
+import { createWebSocketStream, WebSocket } from 'ws';
 
 import robot from 'robotjs';
 
 import { move_mouse } from '../commands/move.mouse';
 import { takeScreenshot } from '../commands/prnt_screen/screenshot';
 
-export const connection = async (ws: any) => {
+export const connection = async (ws: WebSocket) => {
     const wsStream = createWebSocketStream(ws, { encoding: "utf-8", decodeStrings: false });
     wsStream.on("data", async (chunk) => {
         const [method, ...value] = chunk?.split(' ');
         const { x, y } = robot.getMousePos();
         try {
             if (method === 'prnt_scrn') {
-                await takeScreenshot(wsStream, method, x, y, 250, 250)
+                await takeScreenshot(wsStream, method, x, y)
                 return;
             }
             if (method !== 'mouse_position') {
                 move_mouse(method, x, y, value);
             }
-            wsStream.write(`${chunk} ${x},${y}\0`);
+            wsStream.write(`${chunk} ${x},${y} \0`);
+            console.info(`result: ${chunk}`);
         } catch (error) {
             wsStream.write("Invalid command");
         }
+    });
+    ws.on('close', () => {
+        console.info('User logged out!')
+        wsStream.destroy();
     })
 }
